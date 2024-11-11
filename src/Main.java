cae5e4bfd446c965d7ad41888fb506248476f792
JonatanIsraelNachum <gonatan1996@gmail.com>
2024-11-11 23:05:35 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
 import Player.GameFrame;
 import Player.GamePanel;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         GameFrame gameFrame = new GameFrame();
+
+//        JFrame jFrame = new JFrame();
+//        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        jFrame.setSize(500,500);
+//        Image image = ImageIO.read(new File("C:\\Users\\gonat\\Downloads\\Image for Game Pac Man\\Ghost.jpg"));
+//        JLabel jLabel = new JLabel(new ImageIcon(image));
+//        jFrame.add(jLabel);
+//        jFrame.setVisible(true);
+
     }
 }
--- src/Player/GameFrame.java
+++ src/Player/GameFrame.java
@@ -6,11 +6,11 @@ import java.awt.*;
 public class GameFrame extends JFrame {
     public GameFrame() {
         GamePanel gamePanel = new GamePanel();
-        gamePanel.setBackground(Color.BLUE);
+        gamePanel.setBackground(Color.black);
         JFrame frame = new JFrame("Pac Man");
-        frame.setSize(700,700);
+        frame.setSize(775,800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
+        //frame.setLayout(null);
         frame.getContentPane().setBackground(Color.BLACK);
         frame.setVisible(true);
         gamePanel.setBounds(0,0, 50 , 50);
